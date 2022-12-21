4a202362b4e32e6710c44d738bcf3d4f30538487
GridPagma <george.moya01@utrgv.edu>
2022-12-21 11:30:08 -0600

    healthz

--- index.js
+++ index.js
@@ -26,7 +26,27 @@ const { connect } = require("mongoose");
 connect(process.env.DATABASEURL, {}).then(() =>
   console.log("The client is now connected to the database.")
 );
+//
+const express = require("express");
+const http = require("http");
 
+const app = express();
+const router = express.Router();
+
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "GET");
+  next();
+});
+
+router.get("/healthz", (req, res) => {
+  res.status(200).send("Ok");
+});
+
+app.use("/api/v1", router);
+
+const server = http.createServer(app);
+server.listen(3000);
+//
 LoadEvents(client);
 
 client.login(process.env.BOT_TOKEN);
